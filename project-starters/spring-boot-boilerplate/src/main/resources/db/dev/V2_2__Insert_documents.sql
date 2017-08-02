b7d9cb3748060c8b5d9055ac8b8f6da706eabe15
Andreea Mirea <andreea.mirea>
2017-08-02 15:53:28 +0300

    fixed sql script

--- project-starters/spring-boot-boilerplate/src/main/resources/db/dev/V2_2__Insert_documents.sql
+++ project-starters/spring-boot-boilerplate/src/main/resources/db/dev/V2_2__Insert_documents.sql
@@ -1,5 +1,5 @@
 INSERT INTO `document` (`id`, `name`, `description`, `url`)
 VALUES
 	(1, 'Document 1', 'Document 1 description', 'https://www.someurl.com/document1.pdf'),
-	(1, 'Document 2', 'Document 2 description', 'https://www.someurl.com/document2.pdf'),
-	(1, 'Document 3', 'Document 3 description', 'https://www.someurl.com/document3.pdf');
+	(2, 'Document 2', 'Document 2 description', 'https://www.someurl.com/document2.pdf'),
+	(3, 'Document 3', 'Document 3 description', 'https://www.someurl.com/document3.pdf');
