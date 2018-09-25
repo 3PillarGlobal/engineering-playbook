b36a54e7fe29b5e0438ee11affe8aad98af8ba92
Radu <cradudan@yahoo.com>
2018-09-25 14:03:12 +0300

    seed more documents

--- project-starters/spring-boot-boilerplate/src/main/resources/db/dev/V2_2__Insert_documents.sql
+++ project-starters/spring-boot-boilerplate/src/main/resources/db/dev/V2_2__Insert_documents.sql
@@ -2,4 +2,13 @@ INSERT INTO `document` (`id`, `name`, `description`, `url`)
 VALUES
 	(1, 'Document 1', 'Document 1 description', 'https://www.someurl.com/document1.pdf'),
 	(2, 'Document 2', 'Document 2 description', 'https://www.someurl.com/document2.pdf'),
-	(3, 'Document 3', 'Document 3 description', 'https://www.someurl.com/document3.pdf');
+	(3, 'Document 3', 'Document 3 description', 'https://www.someurl.com/document3.pdf'),
+	(4, 'Document 4', 'Document 4 description', 'https://www.someurl.com/document4.pdf'),
+	(5, 'Document 5', 'Document 5 description', 'https://www.someurl.com/document5.pdf'),
+	(6, 'Document 6', 'Document 6 description', 'https://www.someurl.com/document6.pdf'),
+	(7, 'Document 7', 'Document 7 description', 'https://www.someurl.com/document7.pdf'),
+	(8, 'Document 8', 'Document 8 description', 'https://www.someurl.com/document8.pdf'),
+	(9, 'Document 9', 'Document 9 description', 'https://www.someurl.com/document9.pdf'),
+	(10, 'Document 10', 'Document 10 description', 'https://www.someurl.com/document10.pdf'),
+	(11, 'Document 11', 'Document 11 description', 'https://www.someurl.com/document11.pdf'),
+	(12, 'Document 12', 'Document 12 description', 'https://www.someurl.com/document12.pdf');
